1e13225bd44ce337057e00adebf593405271e903
Andrew Hummel <andrewhummel22@gmail.com>
2025-10-10 10:05:22 -0400

    cast-float-first

--- dbt_project/models/staging/stg_attack_results.sql
+++ dbt_project/models/staging/stg_attack_results.sql
@@ -1,10 +1,10 @@
 select
 
     eventid as event_id
-    , nkill::int AS num_killed
-    , nperps::int as num_perpetrators
-    , nwound::int AS num_wounded
-    , property::int as num_properties_damaged
+    , nkill::float::int AS num_killed
+    , nperps::float::int as num_perpetrators
+    , nwound::float::int AS num_wounded
+    , property::float::int as num_properties_damaged
     , (success = 1) AS was_successful
     , (suicide = 1) AS had_suicide
     , CASE
